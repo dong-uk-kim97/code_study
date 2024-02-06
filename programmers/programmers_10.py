b17c2690cdfff21a8033a793e5c3568155956662
DongUk Kim <kdw24739577@gmail.com>
2024-02-06 12:00:00 +0900

    commit

---
+++ programmers/programmers_10.py
@@ -0,0 +1,8 @@
+"""
+정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+list 요소값 전체 더하기 : sum()
+list 요소 갯수 : len()
+"""
+
+def solution(numbers):
+    return sum(numbers) / len(numbers)
