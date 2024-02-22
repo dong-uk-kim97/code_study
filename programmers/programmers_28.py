741675e260004e2748edcd76fec3b1c8ead1e7a4
DongUk Kim <kdw24739577@gmail.com>
2024-02-22 12:00:00 +0900

    commit

---
+++ programmers/programmers_28.py
@@ -0,0 +1,7 @@
+"""
+정수 리스트 num_list와 정수 n이 주어질 때, 
+num_list의 첫 번째 원소부터 n 번째 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+"""
+
+def solution(num_list, n):
+    return num_list[:n] # n번쨰 원소 까지 담은 리스트 return
