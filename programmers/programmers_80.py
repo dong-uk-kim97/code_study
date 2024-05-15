ffd007d1e4d92f78ab81376990729b06cc5f8c0a
DongUk Kim <kdw24739577@gmail.com>
2024-05-15 12:00:00 +0900

    commit

--- programmers/programmers_80.py
+++ programmers/programmers_80.py
@@ -1,6 +1,7 @@
 """
-오늘 해야 할 일이 담긴 문자열 배열 todo_list와 각각의 일을 지금 마쳤는지를 나타내는 boolean 배열 finished가 매개변수로 주어질 때, 
-todo_list에서 아직 마치지 못한 일들을 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+문자열 배열 strArr가 주어집니다. 
+모든 원소가 알파벳으로만 이루어져 있을 때, 배열에서 홀수번째 인덱스의 문자열은 모든 문자를 대문자로, 
+짝수번째 인덱스의 문자열은 모든 문자를 소문자로 바꿔서 반환하는 solution 함수를 완성해 주세요.
 """
 def solution(todo_list, finished):
     return [val for idx, val in enumerate(todo_list) if finished[idx]==False]
