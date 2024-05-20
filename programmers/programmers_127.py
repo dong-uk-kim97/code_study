628d59b79a4a74cc5642f89ac5c7c55b01e70160
DongUk Kim <kdw24739577@gmail.com>
2024-05-20 13:58:00 +0900

    feat : 127.py commit

---
+++ programmers/programmers_127.py
@@ -0,0 +1,8 @@
+"""
+정수 배열 numbers가 매개변수로 주어집니다. 
+numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+"""
+
+def solution(numbers):
+    answer = sorted(numbers)
+    return answer[-1]*answer[-2]
