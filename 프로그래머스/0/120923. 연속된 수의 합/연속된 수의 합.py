10912305e12ce2a101ccf3e28841afd75365cde0
DongUk Kim <kdw24739577@gmail.com>
2024-06-29 14:51:26 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -0,0 +1,7 @@
+def solution(num, total):
+    answer = []
+    var = sum(range(num+1))
+    diff = total - var
+    start_num = diff//num
+    answer = [i+1+start_num for i in range(num)]
+    return answer
