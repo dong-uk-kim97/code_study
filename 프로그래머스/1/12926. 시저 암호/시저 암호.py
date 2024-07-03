95c34d135673e91f9ad3bddab6493665c73a7922
DongUk Kim <kdw24739577@gmail.com>
2024-07-04 07:34:58 +0900

    Update 시저 암호.py
    한글 주석 추가

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,10 +1,10 @@
 def solution(s, n):
-    s = list(s)
+    s = list(s)  # 문자열 s를 리스트로 변환
     
-    for i in range(len(s)):
-        if s[i].isupper():
-            s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))
-        elif s[i].islower():
-            s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
+    for i in range(len(s)):  # s의 각 문자에 대해 반복
+        if s[i].isupper():  # 해당 문자가 대문자인 경우
+            s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))  # 대문자를 n만큼 이동
+        elif s[i].islower():  # 해당 문자가 소문자인 경우
+            s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))  # 소문자를 n만큼 이동
 
-    return "".join(s)
+    return "".join(s)  # 리스트를 문자열로 변환하여 반환
