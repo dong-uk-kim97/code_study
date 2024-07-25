50cb0a7c5af6dfc269dc9e01564794e042d5230a
DongUk Kim <kdw24739577@gmail.com>
2024-07-25 13:17:18 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,7 @@
+-- 코드를 작성해주세요
+SELECT DISTINCT a.ID, a.EMAIL, a.FIRST_NAME, a.LAST_NAME
+FROM DEVELOPERS a, SKILLCODES b
+WHERE 
+    (a.SKILL_CODE & b.CODE) > 0
+    AND b.NAME IN ("Python", "C#")
+ORDER BY a.ID;
