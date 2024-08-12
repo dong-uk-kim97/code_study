47245fa7c6aea4e60de273703a998ca004ae1cfe
DongUk Kim <kdw24739577@gmail.com>
2024-08-12 18:27:04 +0900

    Create 완주하지 못한 선수2.py

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수2.py
@@ -0,0 +1,9 @@
+def solution(participant, completion):
+    value = 0
+    answer = {}
+    for part in participant:
+        answer[hash(part)] = part
+        value += int(hash(part))
+    for comp in completion:
+        value -= hash(comp)
+    return answer[value]
