514931561a27233eb20cb2e5eec0428afc5095c6
DongUk Kim <kdw24739577@gmail.com>
2024-08-12 18:28:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 51.42 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,9 @@
 def solution(participant, completion):
+    value = 0
     answer = {}
-    for i in participant:
-        answer[i] = answer.get(i,0) + 1
-    for j in completion:
-        answer[j] -= 1
-    for k in answer:
-        if answer[k]:
-            return k
+    for part in participant:
+        answer[hash(part)] = part
+        value += int(hash(part))
+    for comp in completion:
+        value -= hash(comp)
+    return answer[value]
