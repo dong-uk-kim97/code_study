f020c008eeba4ed6d0ed999087d7c50ddbe631e2
DongUk Kim <kdw24739577@gmail.com>
2024-08-14 13:47:25 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 2.10 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -0,0 +1,21 @@
+from itertools import permutations
+def calc(priority, n, expression):
+    if n == 2:
+        return str(eval(expression))
+    if priority[n] == '*':
+        res = eval('*'.join([calc(priority, n + 1, e) for e in expression.split('*')]))
+    if priority[n] == '+':
+        res = eval('+'.join([calc(priority, n + 1, e) for e in expression.split('+')]))
+    if priority[n] == '-':
+        res = eval('-'.join([calc(priority, n + 1, e) for e in expression.split('-')]))
+    return str(res)
+
+
+def solution(expression):
+    answer = 0
+    priorities = (list(permutations(['*','-','+'], 3)))
+    for priority in priorities:
+        res = int(calc(priority, 0, expression))
+        answer = max(answer, abs(res))
+
+    return answer
